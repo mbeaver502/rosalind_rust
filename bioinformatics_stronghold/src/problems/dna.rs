c97cc65ca4e655db545d66934834d39d4dec2c1f
mbeaver502 <jmbeaver1@gmail.com>
2023-02-18 22:20:25 -0600

    DNA problem module

---
+++ bioinformatics_stronghold/src/problems/dna.rs
@@ -0,0 +1,28 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use crate::dna::dna::DNA;
+
+pub fn dna(fname: &str) {
+    match parse_single_input(fname) {
+        Err(e) => eprintln!("{}", e),
+        Ok(s) => {
+            let d = DNA::new(&s);
+            println!("{}", d.count_nucleotides());
+        }
+    }
+}
+
+type GenericResult<T> = Result<T, Box<dyn Error>>;
+
+fn parse_single_input(fname: &str) -> GenericResult<String> {
+    let mut file = BufReader::new(File::open(fname)?);
+    let mut buf = String::new();
+
+    file.read_line(&mut buf)?;
+
+    Ok(buf)
+}
