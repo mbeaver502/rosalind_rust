a7bbdf29e11a0491755f898cda776c79a2a19deb
mbeaver502 <jmbeaver1@gmail.com>
2023-02-18 23:06:05 -0600

    Use util submodule

--- bioinformatics_stronghold/src/problems/dna.rs
+++ bioinformatics_stronghold/src/problems/dna.rs
@@ -1,28 +1,13 @@
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use super::util;
 
 use crate::dna::dna::DNA;
 
 pub fn dna(fname: &str) {
-    match parse_single_input(fname) {
+    match util::parse_single_input(fname) {
         Err(e) => eprintln!("{}", e),
         Ok(s) => {
             let d = DNA::new(&s);
             println!("{}", d.count_nucleotides());
         }
     }
-}
-
-type GenericResult<T> = Result<T, Box<dyn Error>>;
-
-fn parse_single_input(fname: &str) -> GenericResult<String> {
-    let mut file = BufReader::new(File::open(fname)?);
-    let mut buf = String::new();
-
-    file.read_line(&mut buf)?;
-
-    Ok(buf.trim().to_string())
 }
--- bioinformatics_stronghold/src/problems/rna.rs
+++ bioinformatics_stronghold/src/problems/rna.rs
@@ -1,14 +1,10 @@
 use crate::dna::dna::DNA;
 use crate::rna::rna::RNA;
 
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use super::util;
 
 pub fn rna(fname: &str) {
-    match parse_single_input(fname) {
+    match util::parse_single_input(fname) {
         Err(e) => eprintln!("{}", e),
         Ok(s) => {
             let d = DNA::new(&s);
@@ -16,15 +12,4 @@ pub fn rna(fname: &str) {
             println!("{}", r);
         }
     }
-}
-
-type GenericResult<T> = Result<T, Box<dyn Error>>;
-
-fn parse_single_input(fname: &str) -> GenericResult<String> {
-    let mut file = BufReader::new(File::open(fname)?);
-    let mut buf = String::new();
-
-    file.read_line(&mut buf)?;
-
-    Ok(buf.trim().to_string())
 }
