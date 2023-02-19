aebc61dda463a0dc987407565ed9ad5b8ec40e60
mbeaver502 <jmbeaver1@gmail.com>
2023-02-18 22:55:10 -0600

    Use the rna problem module

--- bioinformatics_stronghold/src/main.rs
+++ bioinformatics_stronghold/src/main.rs
@@ -1,8 +1,8 @@
-use problems::dna::dna;
-
 pub mod dna;
 pub mod problems;
+pub mod rna;
 
 fn main() {
-    dna("./tests/inputs/rosalind_dna_1_dataset.txt");
+    //problems::dna::dna("./tests/inputs/dna/rosalind_dna_1_dataset.txt");
+    problems::rna::rna("./tests/inputs/rna/rosalind_rna_3_dataset.txt");
 }
